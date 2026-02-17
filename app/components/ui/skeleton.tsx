8e1fef6df7fcc4fa4387a10ede253c147c515c86
Olya Vo <olga.volkova@email.cz>
2026-02-17 02:21:14 +0100

    add some ui components and update existing ones

---
+++ app/components/ui/skeleton.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from "react";
+import { cn } from "@/app/lib/utils";
+
+function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };

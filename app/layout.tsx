fd6c18f0312647e1900e9c046bfbf7b776510916
Olya Vo <olga.volkova@email.cz>
2026-02-22 18:44:14 +0100

    remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "@copilotkit/react-ui/styles.css";
 import { ReactNode } from "react";
 
 const geistSans = localFont({
